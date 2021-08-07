ddc3a1981a4bedc4d2392c80a7da69d648128cac
vismayatk002 <vismayatk002@gmail.com>
2021-08-07 19:36:20 +0530

    Added part time wage

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -1,8 +1,11 @@
 public class EmployeeWageMain{
 
     static final int IS_PRESENT = 1;
-    static final int WAGE_PER_HR=20;
-    static final int FULL_TIME_HR=8;
+    static final int WAGE_PER_HR = 20;
+    static final int FULL_TIME_HR = 16;
+    static final int PART_TIME_HR = 8;
+    static final int FULL_TIME = 1;
+    
     int dailyEmpWage = 0;
 
     public boolean checkIsPresent(double empCheck){
@@ -13,23 +16,27 @@ public class EmployeeWageMain{
             return false;
         }
     }
-    public int calcDailyEmpWage(){
-        dailyEmpWage =  WAGE_PER_HR * FULL_TIME_HR;
-        return  dailyEmpWage; 
+    public void calcDailyEmpWage(int empType){
+        
+        if(empType == FULL_TIME){
+            dailyEmpWage =  WAGE_PER_HR * FULL_TIME_HR;
+            System.out.println("Full time Daily Employee Wage :" + dailyEmpWage);
+        }
+        else{
+            dailyEmpWage =  WAGE_PER_HR * PART_TIME_HR;
+            System.out.println("Part time Daily Employee Wage :" + (dailyEmpWage));
+        }
     }
-
     public static void main(String[] args){
     
         EmployeeWageMain emp = new EmployeeWageMain();
-
         int empCheck = (int)(Math.random() * 10) % 2;
-
+        int empType = (int)(Math.random() * 10) % 2;
         if(emp.checkIsPresent(empCheck)){
-           
-            System.out.println("Daily Employee Wage :" +  emp.calcDailyEmpWage());
+            emp.calcDailyEmpWage(empType);
         }
         else{
             System.out.println("Employee is absent");
-        };
+        }
     }
 }
