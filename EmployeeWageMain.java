39726f0235cdcb7e6585db1b3859a0a80db8d5ed
vismayatk002 <vismayatk002@gmail.com>
2021-08-07 20:07:01 +0530

    Updated monthly wage with condition

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -7,9 +7,10 @@ public class EmployeeWageMain{
     static final int PART_TIME_HR = 8;
     static final int FULL_TIME = 1;
 
-    
     int dailyEmpWage = 0;
     int monthlyWage = 0;
+    int totalHr = 0;
+    int totalAttendance = 0;
 
     public boolean checkIsPresent(double empCheck){
         if(empCheck == IS_PRESENT){
@@ -22,23 +23,27 @@ public class EmployeeWageMain{
     public int calcDailyEmpWage(int empType){
         
         switch(empType){
-            case FULL_TIME :
+            case  FULL_TIME :
+                totalHr += FULL_TIME_HR;
                 dailyEmpWage =  WAGE_PER_HR * FULL_TIME_HR;
             break;
             default :
-                dailyEmpWage =  WAGE_PER_HR * PART_TIME_HR;
+                totalHr += PART_TIME_HR; 
+                dailyEmpWage = WAGE_PER_HR * PART_TIME_HR;
         }
         return dailyEmpWage;
     }
     public int calcMonthlyWage(){
         
         int dailyWage = 0;
-        for(int day=1; day<=WORKING_DAYS; day++){
-            int empCheck = (int)(Math.random() * 10) % 2;
-            if(checkIsPresent(empCheck)){    
-                int empType = (int)(Math.random() * 10) % 2;
-                dailyWage = calcDailyEmpWage(empType);   
-                monthlyWage += dailyWage;
+        while(totalAttendance <=20 && totalHr <=100){
+            for(int day=1; day<=WORKING_DAYS; day++){
+                int empCheck = (int)(Math.random() * 10) % 2;
+                if(checkIsPresent(empCheck)){
+                    int empType = (int)(Math.random() * 10) % 2;
+                    dailyWage = calcDailyEmpWage(empType);   
+                    monthlyWage += dailyWage;
+                }
             }
         }
         return monthlyWage;
