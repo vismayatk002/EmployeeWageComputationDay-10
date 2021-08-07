35f10835186496e8bbde0a14e31e1b51f79aead4
vismayatk002 <vismayatk002@gmail.com>
2021-08-07 19:22:02 +0530

    Added daily Employee wage

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -1,19 +1,35 @@
 public class EmployeeWageMain{
 
-    final int IS_PRESENT = 1;
+    static final int IS_PRESENT = 1;
+    static final int WAGE_PER_HR=20;
+    static final int FULL_TIME_HR=8;
+    int dailyEmpWage = 0;
 
-    public void checkIsPresent(int empCheck){
-        if(empCheck == IS_PRESENT)
-            System.out.println("\nEmployee is Present");
-        else
-            System.out.println("\nEmployee is Absent"); 
+    public boolean checkIsPresent(double empCheck){
+        if(empCheck == IS_PRESENT){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int calcDailyEmpWage(){
+        dailyEmpWage =  WAGE_PER_HR * FULL_TIME_HR;
+        return  dailyEmpWage; 
     }
 
     public static void main(String[] args){
     
-        EmployeeWageMain wage = new EmployeeWageMain();
+        EmployeeWageMain emp = new EmployeeWageMain();
 
         int empCheck = (int)(Math.random() * 10) % 2;
-        wage.checkIsPresent(empCheck);
+
+        if(emp.checkIsPresent(empCheck)){
+           
+            System.out.println("Daily Employee Wage :" +  emp.calcDailyEmpWage());
+        }
+        else{
+            System.out.println("Employee is absent");
+        };
     }
 }
